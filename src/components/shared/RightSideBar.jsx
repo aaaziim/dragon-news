04d3d76163ed31ae7ac7aa7b05f1d7f6b9d500dc
aaaziim <azim210215@gmail.com>
2024-12-21 00:07:47 +0600

    Right Side Bar Login With Google Github Button Style Fixed

--- src/components/shared/RightSideBar.jsx
+++ src/components/shared/RightSideBar.jsx
@@ -6,14 +6,15 @@ const RightSideBar = () => {
             <h2 className="text-2xl font-semibold">Login With</h2>
             <div>
             <div>
-            <button className="btn bg-transparent px-4 py-2  rounded-lg text-cyan-400 border border-cyan-500 my-3 w-full">
-                <FaGoogle  className='text-cyan-400'></FaGoogle>
+            <button className="btn bg-transparent  hover:bg-black px-4 py-2  rounded-lg hover:text-cyan-400 border hover:border-cyan-500 my-3 w-full shadow-xl">
+                <FaGoogle  className='hover:text-cyan-400'></FaGoogle>
                 Google
             </button>
             </div>
             <div>
-            <button className="btn bg-transparent px-4 py-2  rounded-lg text-cyan-400 border border-cyan-500 my-3 w-full">
-                <FaGithub  className='text-cyan-400'></FaGithub>
+            <button className="btn bg-transparent
+            hover:bg-black px-4 py-2  rounded-lg hover:text-cyan-400 border hover:border-cyan-500 my-3 w-full shadow-xl">
+                <FaGithub  className='hover:text-cyan-400'></FaGithub>
                 Github
             </button>
             </div>
