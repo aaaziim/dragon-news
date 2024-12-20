485469db541bfa2a02d6cb9682d7c32236837d91
aaaziim <azim210215@gmail.com>
2024-12-21 00:04:01 +0600

    Right Side Bar Login With Google Github Button Added

--- src/components/shared/RightSideBar.jsx
+++ src/components/shared/RightSideBar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { FaGoogle, FaGithub } from 'react-icons/fa';
 const RightSideBar = () => {
     return (
         <div>
-            <h2 className="text-3xl">right sidebar</h2>
+            <h2 className="text-2xl font-semibold">Login With</h2>
+            <div>
+            <div>
+            <button className="btn bg-transparent px-4 py-2  rounded-lg text-cyan-400 border border-cyan-500 my-3 w-full">
+                <FaGoogle  className='text-cyan-400'></FaGoogle>
+                Google
+            </button>
+            </div>
+            <div>
+            <button className="btn bg-transparent px-4 py-2  rounded-lg text-cyan-400 border border-cyan-500 my-3 w-full">
+                <FaGithub  className='text-cyan-400'></FaGithub>
+                Github
+            </button>
+            </div>
+            </div>
         </div>
     );
 };
